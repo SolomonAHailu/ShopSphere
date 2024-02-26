d26846daa85640031ebf9b92084704bc719db66a
Solomon Asregdew <solomonasregdew1992@gmail.com>
2024-02-26 23:18:05 +0300

    project commit

---
+++ src/components/AllData.js
@@ -0,0 +1,323 @@
+import Img1 from "../img/products/p1.jpg";
+import Img2 from "../img/products/p2.jpg";
+import Img3 from "../img/products/p3.jpg";
+import Img4 from "../img/products/p4.jpg";
+import Img5 from "../img/products/p5.jpg";
+import Img6 from "../img/products/p6.jpg";
+import Img7 from "../img/products/p7.jpg";
+import Img8 from "../img/products/p8.jpg";
+import Img9 from "../img/products/p9.jpg";
+import Img10 from "../img/products/p10.jpg";
+import Img11 from "../img/products/p11.jpg";
+import Img12 from "../img/products/p12.jpg";
+import Img13 from "../img/products/p13.jpg";
+import Img14 from "../img/products/p14.jpg";
+import Img15 from "../img/products/p15.jpg";
+import Img16 from "../img/products/p16.jpg";
+import Img17 from "../img/products/p17.jpg";
+import Img18 from "../img/products/p18.jpg";
+import Img19 from "../img/products/p19.jpg";
+import Img20 from "../img/products/p20.jpg";
+import OtherImgs1 from "../img/products/otherProducts/other1.jpg";
+import OtherImgs2 from "../img/products/otherProducts/other2.jpg";
+import Toster1 from "../img/products/otherProducts/toster1.jpg";
+import Toster2 from "../img/products/otherProducts/toster2.jpg";
+import LampBlue1 from "../img/products/otherProducts/lampblue1.webp";
+import LampBlue2 from "../img/products/otherProducts/lampblue2.webp";
+import PulpUnit1 from "../img/products/otherProducts/pulpunit1.jpg";
+import PulpUnit2 from "../img/products/otherProducts/pulpunit2.jpg";
+import ModernGold1 from "../img/products/otherProducts/moderngold1.webp";
+import ModernGold2 from "../img/products/otherProducts/moderngold2.webp";
+import CleaningOil1 from "../img/products/otherProducts/cleaningoil1.jpg";
+import CleaningOil2 from "../img/products/otherProducts/cleaningoil2.jpg";
+import BlackAndWhite1 from "../img/products/otherProducts/blackandwhite1.webp";
+import BlackAndWhite2 from "../img/products/otherProducts/blackandwhite2.webp";
+import BodyOil1 from "../img/products/otherProducts/bodyoil1.jpg";
+import BodyOil2 from "../img/products/otherProducts/bodyoil2.jpg";
+import Shelf1 from "../img/products/otherProducts/shelf1.webp";
+import Shelf2 from "../img/products/otherProducts/shelf2.webp";
+import Vase1 from "../img/products/otherProducts/vase1.webp";
+import Vase2 from "../img/products/otherProducts/vase2.webp";
+import ChairBoom1 from "../img/products/otherProducts/chairboom1.webp";
+import ChairBoom2 from "../img/products/otherProducts/chairboom2.webp";
+import BlackLamp1 from "../img/products/otherProducts/blacklamp1.webp";
+import BlackLamp2 from "../img/products/otherProducts/blacklamp2.jpg";
+import GoldenLamp1 from "../img/products/otherProducts/goldenlamp1.webp";
+import GoldenLamp2 from "../img/products/otherProducts/goldenlamp2.webp";
+import Cotton1 from "../img/products/otherProducts/cotton1.jpg";
+import Cotton2 from "../img/products/otherProducts/cotton2.jpg";
+import WoodChair1 from "../img/products/otherProducts/woodchair1.webp";
+import WoodChair2 from "../img/products/otherProducts/woodchair2.webp";
+import Copenhagen1 from "../img/products/otherProducts/copenhagen1.webp";
+import Copenhagen2 from "../img/products/otherProducts/copenhagen2.webp";
+import Cam1 from "../img/products/otherProducts/cam1.webp";
+import Cam2 from "../img/products/otherProducts/cam2.jpg";
+import TableLamp1 from "../img/products/otherProducts/tablelamp1.webp";
+import TableLamp2 from "../img/products/otherProducts/tablelamp2.webp";
+import MatLamp1 from "../img/products/otherProducts/mat1.webp";
+import MatLamp2 from "../img/products/otherProducts/mat2.webp";
+import Comfy1 from "../img/products/otherProducts/comfy1.webp";
+import Comfy2 from "../img/products/otherProducts/comfy2.webp";
+
+export const items = [
+  {
+    id: 1,
+    category: "chair",
+    img: Img1,
+    description: "Little Armchair Sheepskin",
+    price: 986,
+    otherImgs: [OtherImgs1, OtherImgs2],
+    specs:
+      "Little Petra VB1 Armchair in Traditional Sheepskin Moonlight with Walnut and Brass Finish - Limited Edition. Enjoy an exclusive discount when you add this product to your basket.",
+    texture: "Sheep Skin",
+    weight: "15kg",
+    size: "150cm x 70cm",
+  },
+  {
+    id: 2,
+    category: "kitchen",
+    img: Img2,
+    description: "Pop-Up Toaster",
+    price: 45,
+    otherImgs: [Toster1, Toster2],
+    specs:
+      "The toaster's rounded square design enables it to neatly fit against a wall or inside a corner. It excels at defrosting and maintains the ideal distance between the bread and the heater, resulting in crunchy toast with a soft, spongey middle.",
+    texture: "Plastic",
+    weight: "3.5kg",
+    size: "20xm x 10cm",
+  },
+  {
+    id: 3,
+    category: "lamp",
+    img: Img3,
+    description: "Lamp Light Blue",
+    price: 79,
+    otherImgs: [LampBlue1, LampBlue2],
+    specs:
+      "Originally created in 1968, the Verner Panton Flowerpot was initially designed for the restaurant industry and Verner Panton exhibitions.",
+    texture: "Plastic",
+    weight: "2kg",
+    size: "15cm x 15cm",
+  },
+  {
+    id: 4,
+    category: "furniture",
+    img: Img4,
+    description: "Pulp Unit - 5 Compartments",
+    price: 254,
+    otherImgs: [PulpUnit1, PulpUnit2],
+    specs:
+      "Lightweight and durable, Pulp Units are versatile and can be used either horizontally or vertically. Crafted with A4 paper in mind, these units neatly accommodate MUJI angle files and ring binders.",
+    texture: "Wood",
+    weight: "45kg",
+    size: "250cm x 60cm",
+  },
+  {
+    id: 5,
+    category: "lamp",
+    img: Img5,
+    description: "Golden Modern Light",
+    price: 189,
+    otherImgs: [ModernGold1, ModernGold2],
+    specs:
+      "Tom Dixon and FRONT have collaboratively crafted an exceptionally beautiful pendant with a completely unique design. Utilizing a special metallization technology, the lamp boasts a distinct appearance, marking it as the first of its kind. When illuminated, the lamp becomes transparent, revealing an exciting play of colors.",
+    texture: "Plastic",
+    weight: "3kg",
+    size: "20cm x 20cm",
+  },
+  {
+    id: 6,
+    category: "skin-care",
+    img: Img6,
+    description: "Body Oil 200ml",
+    price: 67,
+    otherImgs: [CleaningOil1, CleaningOil2],
+    specs:
+      "Crafted from 100% plant-based oils with olive oil as the primary ingredient, this cleansing oil offers a gentle and skin-friendly solution. Free from fragrance, colors, mineral oils, parabens, and alcohol, it undergoes allergy testing (though individual reactions cannot be guaranteed). To use, take a small amount in the palm, massage it onto the face to effectively remove makeup and dirt, and rinse thoroughly with cold or warm water.",
+    texture: "Liquid",
+    weight: "200ml",
+    size: "10cm x 5cm",
+  },
+  {
+    id: 7,
+    category: "lamp",
+    img: Img7,
+    description: "Black and White Lamp",
+    price: 220,
+    otherImgs: [BlackAndWhite1, BlackAndWhite2],
+    specs:
+      "The concept behind Dot draws inspiration from perforated metal and the intricate patterns formed when light filters through the small holes. The juxtaposition of the coarse perforated metal and the smooth opal glass gives Dot a simple yet meaningful pendant design. Whether the light is on or off, Dot exudes an elegant expression, showcasing its timeless charm.",
+    texture: "Plastic",
+    weight: "6kg",
+    size: "20cm x 20cm",
+  },
+  {
+    id: 8,
+    category: "furniture",
+    img: Img8,
+    description: "Gejst Shelf A Black Ash/Black",
+    price: 115,
+    otherImgs: [Shelf1, Shelf2],
+    specs:
+      "Böttcher & Kayser designed Nivo as a minimalist wall shelf that seamlessly combines functionality and aesthetics. This collection features clean lines and lacks unnecessary details, allowing it to hang easily and elegantly on the wall. Nivo provides a calm and organized display for the items you choose to place on the shelf.",
+    texture: "Wood",
+    weight: "10kg",
+    size: "60cm x 20cm",
+  },
+  {
+    id: 9,
+    category: "furniture",
+    img: Img9,
+    description: "Cube Lolo Vase Black",
+    price: 161,
+    otherImgs: [Vase1, Vase2],
+    specs:
+      "Originally designed by Søren Lassen in 2014, the Kubus Vase Lolo was officially launched in 2018 to commemorate the 10th anniversary of by Lassen. This vase serves as a natural and highly coveted extension of the Kubus series, which already includes the Kubus candlesticks and Kubus Bowl. Whether placed alone or as part of a still life arrangement, the vase offers versatility. Fill it with airy, colorful flowers for a feminine touch, or let it stand alone, showcasing its simplicity and precision.",
+    texture: "Ceramics",
+    weight: "12kg",
+    size: "45cm x 15cm",
+  },
+  {
+    id: 10,
+    category: "chair",
+    img: Img10,
+    description: "Traditional Armchair",
+    price: 89,
+    otherImgs: [ChairBoom1, ChairBoom2],
+    specs:
+      "The concept behind &Tradition's Boomerang lounge chair embodies a classic Danish design infused with a modern and minimalist expression. Originally crafted by Hvidt & Mølgaard in 1956, this chair features a timeless appeal with stylish cushions and a hand-polished wooden frame. It represents an ideal Nordic design that imparts a personal touch to any home, showcasing a perfect blend of functionality and aesthetics.",
+    texture: "Wood",
+    weight: "8kg",
+    size: "60cm x 40cm",
+  },
+  {
+    id: 11,
+    category: "lamp",
+    img: Img11,
+    description: "The Sun Pendant Black",
+    price: 160,
+    otherImgs: [BlackLamp1, BlackLamp2],
+    specs:
+      "The Here Comes The Sun pendant, envisioned by Bertrand Balas, is a high-quality lamp distinguished by its unique and magical appearance. The series carries a fantastic expression, particularly when illuminated, casting a special atmosphere in the room where it is suspended. This lamp is an ideal fit above dining room tables, kitchen counters, and as lighting in living rooms, showcasing both functionality and an enchanting aesthetic.",
+    texture: "Plastic",
+    weight: "4kg",
+    size: "20cm x 20cm",
+  },
+  {
+    id: 12,
+    category: "lamp",
+    img: Img12,
+    description: "Simple Golden Lamp",
+    price: 220,
+    otherImgs: [GoldenLamp1, GoldenLamp2],
+    specs:
+      "The Verner Panton Flowerpot, created in 1968, was initially crafted for the hospitality industry and to adorn Verner Panton's displays and showrooms. Over time, the lamp gained popularity in private homes. Thanks to its stylish design and an array of color choices, it has experienced a significant resurgence in recent years.",
+    texture: "Plastic",
+    weight: "4kg",
+    size: "15cm x 15cm",
+  },
+  {
+    id: 13,
+    category: "skin-care",
+    img: Img13,
+    description: "Unbleached Cotton Pads 180",
+    price: 33,
+    otherImgs: [Cotton1, Cotton2],
+    specs:
+      "These cotton pads are crafted from soft, unbleached cotton, originating from Japan and composed entirely of 100% cotton.",
+    texture: "Cotton",
+    weight: "180g",
+    size: "20cm x 10cm",
+  },
+  {
+    id: 14,
+    category: "chair",
+    img: Img14,
+    description: "Oak Spanish Chair",
+    price: 520,
+    otherImgs: [WoodChair1, WoodChair2],
+    specs:
+      "When discussing Børge Mogensen, The Spanish Chair is likely one of the first designs that come to mind. This makes perfect sense, as it stands out as one of his most recognized products worldwide.",
+    texture: "Wood",
+    weight: "15kg",
+    size: "60cm x 30cm",
+  },
+  {
+    id: 15,
+    category: "chair",
+    img: Img15,
+    description: "Copenhagen Armchair",
+    price: 330,
+    otherImgs: [Copenhagen1, Copenhagen2],
+    specs:
+      "The concept behind the Hyg series by Normann Copenhagen draws inspiration from the Danish word 'Hygge.'' Designed by Simon Legald, the series aims to encapsulate the essence of the word, emphasizing feelings of security, warmth, and convenience in furniture. This is manifested through the fine organic shapes and hearty curves present in the chairs, ensuring a high level of sitting comfort and overall well-being.",
+    texture: "Wood",
+    weight: "14kg",
+    size: "65cm x 35cm",
+  },
+  {
+    id: 16,
+    category: "electronic",
+    img: Img16,
+    description: "Anti Dark Light",
+    price: 120,
+    otherImgs: [Cam1, Cam2],
+    specs:
+      "The Easy Mini W75 from Antidark lives up to its name effortlessly. With a simple design, comfortable lighting, and easy adjustability, the Easy Wall Light simplifies the user experience in every aspect. This lamp can rotate 360 degrees and be tipped, offering versatility. Ideal for placement alongside other lamps to illuminate all corners of the room, it serves as both a wall light and a ceiling light, providing flexibility in its use.",
+    texture: "Metal",
+    weight: "2kg",
+    size: "15cm x 10cm",
+  },
+  {
+    id: 17,
+    category: "lamp",
+    img: Img17,
+    description: "Table Lamp",
+    price: 75,
+    otherImgs: [TableLamp1, TableLamp2],
+    specs:
+      "The lamp has gained popularity in private homes, experiencing a notable resurgence in recent years. This can be attributed, in no small part, to the lamp's stylish design and the extensive range of color options, making it a versatile fit for various settings.",
+    texture: "Metal",
+    weight: "5kg",
+    size: "25cm x 15cm",
+  },
+  {
+    id: 18,
+    category: "lamp",
+    img: Img18,
+    description: "Mat Black Lamp",
+    price: 175,
+    otherImgs: [MatLamp1, MatLamp2],
+    specs:
+      "Exclusive Offer: Enjoy a special discount when you add this product to your basket. *Terms apply, valid only for specific products.",
+    texture: "Metal",
+    weight: "6kg",
+    size: "20cm x 15cm",
+  },
+  {
+    id: 19,
+    category: "chair",
+    img: Img19,
+    description: "Comfy Chair",
+    price: 119,
+    otherImgs: [Comfy1, Comfy2],
+    specs:
+      "The Sum series, designed by Danish designer Simon Legald, epitomizes modern elegance. With an elegant and resolute design reminiscent of the tip of a wing, the armrests extend invitingly, creating an embrace in the soft, homely space of the armchair.",
+    texture: "Comfy Material",
+    weight: "25kg",
+    size: "60cm x 30cm",
+  },
+  {
+    id: 20,
+    category: "skin-care",
+    img: Img20,
+    description: "Body Oil 50ml",
+    price: 45,
+    otherImgs: [BodyOil1, BodyOil2],
+    specs:
+      "Crafted from 100% plant-based oils with olive oil as the primary ingredient, this cleansing oil offers a gentle touch for the skin. It is free from fragrance, colors, mineral oils, parabens, and alcohol, and undergoes allergy testing (although individual reactions cannot be guaranteed). To use, take a small amount in the palm of your hand, massage it into the face to remove makeup and dirt, and rinse thoroughly with cold or warm water.",
+    texture: "Liquid",
+    weight: "50ml",
+    size: "10cm x 5cm",
+  },
+];
---
+++ src/components/EmptyCart.jsx
@@ -0,0 +1,14 @@
+// import EmptyCartImg from "../img/cart/empty-cart.png";
+import EmptyCartImg from "../img/cart/pngwing_empty-cart.png";
+
+function EmptyCart({ openCart }) {
+  return (
+    <div className="empty-cart">
+      <img src={EmptyCartImg} alt="empty-cart" />
+      <p>Your cart is empty 🤔</p>
+      <button onClick={openCart}>Keep Browsing</button>
+    </div>
+  );
+}
+
+export default EmptyCart;
